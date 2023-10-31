596d9eed017625d3931540cbe4d001029aaf08ca
Dionei Silva <dionei.silva@gmail.com>
2023-10-31 17:42:23 -0300

    fix exercise 4

--- exercicio4/script.js
+++ exercicio4/script.js
@@ -1,11 +1,13 @@
 /* 4. Reescreva o exercício 3, utilizando apenas o SWITCH. */
+const weekDay = (Number(prompt('insira um dia da semana')))
+
 switch (weekDay) {
-      case  1: console.log(`4. O dia respectivo é Domingo`) ; break;
-      case  2: console.log(`4. O dia respectivo é Segunda`) ; break;
-      case  3: console.log(`4. O dia respectivo é Terça  `) ; break;
-      case  4: console.log(`4. O dia respectivo é Quarta `) ; break;
-      case  5: console.log(`4. O dia respectivo é Quinta `) ; break;
-      case  6: console.log(`4. O dia respectivo é Sexta  `) ; break;
-      case  7: console.log(`4. O dia respectivo é Sábado `) ; break;
-      default: console.log(`4. Dia não reconhecido`) ; break;
+      case  1: document.write(`4. O dia respectivo é Domingo`) ; break;
+      case  2: document.write(`4. O dia respectivo é Segunda`) ; break;
+      case  3: document.write(`4. O dia respectivo é Terça  `) ; break;
+      case  4: document.write(`4. O dia respectivo é Quarta `) ; break;
+      case  5: document.write(`4. O dia respectivo é Quinta `) ; break;
+      case  6: document.write(`4. O dia respectivo é Sexta  `) ; break;
+      case  7: document.write(`4. O dia respectivo é Sábado `) ; break;
+      default: document.write(`4. Dia não reconhecido`) ; break;
   }
